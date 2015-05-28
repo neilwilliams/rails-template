8a2027dc6b3868cc8d1203c22cbbafe78060d5cd
Neil Williams <neil121183@gmail.com>
2015-05-28 15:44:12 +0100

    Update application.js
    Remove jquery from rails application.js. We bundle this with bower.

--- template/assets/javascripts/application.js
+++ template/assets/javascripts/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
 // PLEASE NOTE: This file is only used for the built in rails stuff, we do not want to use this for asset management. We use bower / grunt. Any javascript placed in here should be managed with grunt, and referenced from the public directory.
